523a07a3fc9215cebb37dfaa121f8d145e459dc5
mohebifar <netfars@gmail.com>
2016-02-25 00:25:06 +0330

    refactor: redundant async

--- src/scrappers/venues/index.js
+++ src/scrappers/venues/index.js
@@ -1,7 +1,7 @@
 import { client } from 'services/api';
 import Venue from './venue';
 
-export default async function getVenues() {
+export default function getVenues() {
   return client
     .getVenues()
     .then(data => data.map(venue => new Venue(venue)));
